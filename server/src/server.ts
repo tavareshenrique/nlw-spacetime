e76d446ba964f3a6ebf6cd54a6f2e9146319aa96
Henrique Tavares <ihenrits@gmail.com>
2023-05-17 10:14:05 -0300

    reate server routes and cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,15 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async (request, reply) => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
+
+app.register(memoriesRoutes)
 
 app
   .listen({
