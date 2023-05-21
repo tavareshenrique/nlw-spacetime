fcf53e8ebddd6dc134236f1097f21549407ee647
Henrique Tavares <ihenrits@gmail.com>
2023-05-21 12:14:37 -0300

    create memory in db

---
+++ web/src/components/MediaPicker.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { ChangeEvent, useState } from 'react'
+
+export function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null)
+
+  function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target
+
+    if (!files) {
+      return
+    }
+
+    const previewURL = URL.createObjectURL(files[0])
+
+    setPreview(previewURL)
+  }
+
+  return (
+    <>
+      <input
+        type="file"
+        id="media"
+        name="coverUrl"
+        className="invisible h-0 w-0"
+        accept="image/*"
+        onChange={onFileSelected}
+      />
+
+      {preview && (
+        // eslint-disable-next-line
+        <img
+          src={preview}
+          alt=""
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+      )}
+    </>
+  )
+}
