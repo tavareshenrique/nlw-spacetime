03990132327e5501348bdf8d35ea73dfbf65a8ba
Henrique Tavares <ihenrits@gmail.com>
2023-05-21 13:15:48 -0300

    add link in EmptyMemories

--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -3,7 +3,7 @@ export function EmptyMemories() {
     <div className="flex flex-1 items-center justify-center p-16">
       <p className="w-[360px] text-center leading-relaxed">
         Você ainda não registrou nenhuma lembrança, comece a{' '}
-        <a href="" className="underline hover:text-gray-50">
+        <a href="/memories/new" className="underline hover:text-gray-50">
           criar agora!
         </a>
       </p>
